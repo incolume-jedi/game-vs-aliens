29284bf217eec1b218ec08a43680376b09a6a296
Britodfbr <britodfbr@gmail.com>
2022-08-23 07:50:50 -0300

    feat: movimentação da nave humana na tela de jogo

--- incolume/py/vsaliens/vsaliens.py
+++ incolume/py/vsaliens/vsaliens.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from os import getenv
 from pathlib import Path
 
-
 load_dotenv()
 
 
@@ -24,6 +23,7 @@ tela = Point(int(getenv('WIDTH')), int(getenv('HEIGHT')))
 
 screen = pygame.display.set_mode(tela.to_tuple())
 pygame.display.set_caption(getenv('GAME_NAME', 'Game by Guilda JEDI'))
+# clock = pygame.
 running = True
 imagens = Path(__file__).parents[3].joinpath('imagens')
 
@@ -43,7 +43,6 @@ humanwappon = pygame.transform.scale(humanwappon, (30, 50))
 humanwappon = pygame.transform.rotate(humanwappon, -90)
 pos_humanwappon = Point(200, 200)
 
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,18 +50,24 @@ while running:
     screen.blit(bg1, (0, 0))
     rel_x = tela.x % bg1.get_rect().width
     screen.blit(bg1, (rel_x - bg1.get_rect().width, 0))
-    if rel_x < 1200:
+    if rel_x < bg1.get_rect().width:
         screen.blit(bg1, (rel_x, 0))
     # Teclas
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_humanship.y > 1:
         pos_humanship.y -= 1
-    if tecla[pygame.K_DOWN] and pos_humanship.y < tela.y - 100:
+    if tecla[pygame.K_DOWN] and \
+            pos_humanship.y < tela.y - humanship.get_rect().height:
         pos_humanship.y += 1
-    # if tecla[pygame.K_RIGHT] and pos_humanship.x < tela.x:
-    #     pos_humanship.x += 1
-    # if tecla[pygame.K_LEFT] and pos_humanship.x > 0:
-    #     pos_humanship.x -= 1
+    if tecla[pygame.K_RIGHT] and \
+            (
+                pos_humanship.x <
+                screen.get_rect().width -
+                humanship.get_rect().width
+            ):
+        pos_humanship.x += 1
+    if tecla[pygame.K_LEFT] and pos_humanship.x > 0:
+        pos_humanship.x -= 1
 
     # Movimento da tela
     tela.x -= float(os.getenv('TXMOVE'))
